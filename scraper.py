9688d77f4148da2e484b6531488a10e9823caaa9
Abigail Rumsey <abbie_is@hotmail.com>
2016-01-10 16:45:56 +0000

    Added own scraper code

--- scraper.py
+++ scraper.py
@@ -1,24 +1,56 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+from lxml import html
+import requests
+import scraperwiki
+import time
+import string
+from dateutil.parser import parse
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+def do_the_scraping():
+    page = requests.get('http://www.reading.ac.uk/news-and-events/about-eventlist.aspx')
+    tree = html.fromstring(page.text)
+    links = tree.xpath('//a[contains(@href,"/about/newsandevents/Events/")]/@href')
+    id = 0
+    for link in links:
+        url = 'http://www.reading.ac.uk' + link
+        id += 1
+        try:
+            scrape_event(id,url)
+        except:
+            "Error scraping event: " + url
+        time.sleep(0.5)
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+def scrape_event(id,url):
+    page = requests.get(url)
+    tree = html.fromstring(page.text)
+    title = tree.xpath('//h1[@class="page-header"]/text()')[0]
+    description = tree.xpath('//p[@class="event-summary"]/text()')
+    raw_date = tree.xpath('//p[@class="event-date"]/text()')[0]
+    try:
+        date = parse(raw_date)
+        date = date.strftime('%Y-%m-%d')
+    except:
+        date = raw_date
+    event_time = tree.xpath('//p[@class="event-time"]/text()')[0]
+    location = tree.xpath('//p[@class="event-location"]/text()')[0]
+    try:
+        venue = location.split(',')[0]
+    except:
+        venue = location
+    contact = tree.xpath('//p[@class="event-contact"]/text()')
+    
+    data = {
+        'id': 'uor' + str(id),
+        'title': title,
+        'description': description,
+        'date': date,
+        'time': event_time,
+        'location': location,
+        'venue': venue,
+        'source': url,
+        'contact': contact,        
+    }
+    
+    print data
+    scraperwiki.sqlite.save(unique_keys = ['id'], data = data)
+        
+do_the_scraping()
